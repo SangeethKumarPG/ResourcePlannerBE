4dfc7ba21b7ca0553002fd9c1bb271dfdf8dd0f1
sangeethkumarpg <pgsangeethkumar@gmai.com>
2024-10-21 00:49:09 +0530

    update and delete products completed

--- Controllers/productController.js
+++ Controllers/productController.js
@@ -26,3 +26,38 @@ exports.getAllProducts = async(req,res)=>{
         res.status(401).json("You are not permitted to read products")
     }
 }
+
+exports.editProduct = async(req,res)=>{
+    const userId = req.payload;
+    const currentUser = await users.findOne({_id:userId});
+    if(currentUser.permissions.productsAndServices.write === true){
+        try{
+            const productId = req.params.id;
+            const updatedProduct = await products.findByIdAndUpdate({_id:productId},req.body,{new:true});
+            await updatedProduct.save();
+            res.status(200).json(updatedProduct);
+        }catch(err){
+            console.log(err);
+            res.status(500).json("Internal Server Error");
+        }
+    }else{
+        res.status(401).json("You are not permitted to write into the products");
+    }
+}
+
+exports.deleteProduct = async(req,res)=>{
+    const userId = req.payload;
+    const currentUser = await users.findOne({_id:userId});
+    if(currentUser.permissions.productsAndServices.delete === true){
+        try{
+            const productId = req.params.id;
+            const deletedProduct = await products.findByIdAndDelete({_id:productId});
+            res.status(200).json(deletedProduct);
+        }catch(err){
+            console.log(err);
+            res.status(500).json("Internal Server Error");
+        }
+    }else{
+        res.status(401).json("You are not permitted to delete products");
+    }
+}
--- Routes/routes.js
+++ Routes/routes.js
@@ -10,7 +10,8 @@ const jwtMiddleWare = require('../MiddleWares/jwtMiddleWare');
 router.post('/login', userController.login);
 router.post('/products-and-services', jwtMiddleWare, productController.addProduct)
 router.get('/products-and-services', jwtMiddleWare, productController.getAllProducts);
-
+router.put('/products-and-services/:id', jwtMiddleWare, productController.editProduct);
+router.delete('/products-and-services/:id', jwtMiddleWare, productController.deleteProduct);
  
 
 module.exports = router;
